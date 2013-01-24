082aab3df56c52cbbed4f5c13d6a63292c17ae37
J Irving <j@lollyshouse.ca>
2013-01-24 01:39:27 -0500

    Update some dependencies

--- project.clj
+++ project.clj
@@ -7,19 +7,18 @@
                  [compojure "1.1.3"]
                  [hiccup "1.0.2"]
                  [cheshire "5.0.1"]
-                 [com.novemberain/monger "1.4.1"]
+                 [com.novemberain/monger "1.4.2"]
                  [ring/ring-jetty-adapter "1.1.6"]
                  [clj-time "0.4.4"]
-                 [com.cemerick/friend "0.1.2"]
+                 [com.cemerick/friend "0.1.3"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.10"]
                  [log4j/log4j "1.2.17"]]
   :profiles {:dev {:dependencies [[ring/ring-devel "1.1.6"]
                                   [midje "1.4.0"]
-                                  [com.stuartsierra/lazytest "1.2.3"]]
-                   :plugins [[lein-midje "2.0.3"]]}}
-  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
-  :plugins [[lein-ring "0.7.5"]]
+                                  [bultitude "0.1.7"]]
+                   :plugins [[lein-midje "2.0.4"]]}}
+  :plugins [[lein-ring "0.8.2"]]
   :ring {:handler yoctoplasm-clj.server/yoctoplasm
          :init yoctoplasm-clj.server/init}
   :min-lein-version "2.0.0")
